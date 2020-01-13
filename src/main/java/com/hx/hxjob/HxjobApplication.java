25968784cd7c05f423d1217895849c8d3a798058
yuxiangfan <157326217yuxiangfan@cloudcore.cn>
2020-01-13 16:33:45 +0800

    第一次提交

---
+++ src/main/java/com/hx/hxjob/HxjobApplication.java
@@ -0,0 +1,13 @@
+package com.hx.hxjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HxjobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hx/hxjob/HxjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hx.hxjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HxjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
